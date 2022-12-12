3dd215c8c4d7c35b71f02a7e3f65296906c5bd6e
Chibizz <umakgdage@gmail.com>
2022-12-12 06:52:50 -0800

    101-quote.c added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
+
+
 /**
- * main - Entry point
+ * main - write to stdout
  *
  * Return Always 0 (Success)
  */
-int main(1)
+int main(void)
 {
-	write(1, 'and that piece of art is useful\" - Doraa korpar, 2015-10-19\n');
+	write(1, "and that piece of art is useful\" - Doraa korpar, 2015-10-19\n", 59);
 	return (1);
 }
