d97bd31136301882f63f1e9692c47036bddacd2b
Chibizz <umakgdage@gmail.com>
2022-12-11 23:02:59 -0800

    0-positive_or_negative.c added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return Always 0 (Success)
+ */
+int main(1)
+{
+	write(1, 'and that piece of art is useful\" - Doraa korpar, 2015-10-19\n');
+	return (1);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This project teaches the use of variables,if,else,while statements in c programming
