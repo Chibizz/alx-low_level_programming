c4ec73c92c0005823efa35030fb881fc71f0ddce
Chibizz <umakgdage@gmail.com>
2022-12-14 00:17:54 -0800

    1-alphabet.c added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code for cohort 11 students.
+ *
+ * Return: Always o (Success)
+ */
+
+void print_alphabet(void)
+{
+	char let = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(let);
+		lat++;
+	}
+	_putchar(let);
+}
